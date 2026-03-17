33cbde2b2a7b907a5d7b5170d20185beb70cea80
Pawan Kumar Pandey <pawan4kp@gmail.com>
2026-03-18 01:43:37 +0530

    Todo List almost working

--- script.js
+++ script.js
@@ -15,4 +15,35 @@ OuterElemBackBtn.forEach(function(btn){
     })
 })
 }
-//runFeatures()
+runFeatures()
+
+var taskName = document.querySelector('.addTask input');
+var taskArea = document.querySelector('.addTask textarea');
+var  taskForm = document.querySelector('.addTask form');
+
+var taskCheck = document.querySelector('.addTask form #check')
+
+
+taskForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    currentTask.push({task:taskName.value,isChecked:taskCheck.checked})
+    renderTask();
+})
+
+var currentTask = []
+renderTask();
+
+function renderTask(){
+var allTask = document.querySelector('.allTask')
+var sum='';
+    currentTask.forEach(function(elem){
+        sum = sum+ `
+                <div class="task">
+                    <h4>${elem.task} <span id=${elem.isChecked}>Imp</span></h4>
+                    <button class = 'complete'>Mark as complete</button>
+                </div>`
+    })
+    allTask.innerHTML = sum;
+}
+
+
