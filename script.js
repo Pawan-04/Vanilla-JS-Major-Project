fae6e02c897dca1580ed8eaa0a8a04f3a41834dc
Pawan Kumar Pandey <pawan4kp@gmail.com>
2026-03-19 02:07:48 +0530

    With bug (for delete), Local storage introduced

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function runFeatures(){
     var allElems = document.querySelectorAll('.elem');
 var OuterElem = document.querySelectorAll('.OuterElem')
@@ -16,6 +17,7 @@ OuterElemBackBtn.forEach(function(btn){
 })
 }
 runFeatures()
+// feature
 
 var taskName = document.querySelector('.addTask input');
 var taskArea = document.querySelector('.addTask textarea');
@@ -24,26 +26,51 @@ var  taskForm = document.querySelector('.addTask form');
 var taskCheck = document.querySelector('.addTask form #check')
 
 
+var currentTask = []
+currentTask = JSON.parse(localStorage.getItem('arr'))
+renderTask();
+
 taskForm.addEventListener('submit',function(e){
     e.preventDefault();
-    currentTask.push({task:taskName.value,isChecked:taskCheck.checked})
+    currentTask.push({task:taskName.value,isChecked:taskCheck.checked,taskArea:taskArea.value})
+    localStorage.setItem("arr",JSON.stringify(currentTask))
     renderTask();
 })
 
-var currentTask = []
-renderTask();
 
 function renderTask(){
 var allTask = document.querySelector('.allTask')
 var sum='';
     currentTask.forEach(function(elem){
         sum = sum+ `
+        <div class= "summ">
                 <div class="task">
                     <h4>${elem.task} <span id=${elem.isChecked}>Imp</span></h4>
                     <button class = 'complete'>Mark as complete</button>
-                </div>`
+                </div>
+                <details>
+                    <summary>Task Description</summary>
+                    <p>${elem.taskArea}</p>
+                    <button class ='del'>Delete</button>
+                    </details>
+            </div>`
     })
     allTask.innerHTML = sum;
 }
 
 
+//Delete
+function delte(){
+var del = document.querySelectorAll('.del');
+del.forEach(function(btn,i){
+    btn.addEventListener('click',function(){
+        currentTask.splice(i,1)
+        localStorage.setItem("arr",JSON.stringify(currentTask))
+        renderTask()
+        delte()
+        x()
+
+    })
+});
+}
+delte()
